8bc532150b741422a44b181a74766f505c316c96
carlos Egana <carlos.egana.gutierrez@gmail.com>
2023-11-12 17:24:19 -0300

    004

--- main4.js
+++ main4.js
@@ -1,87 +1,77 @@
-
-document.addEventListener("DOMContentLoaded", async function () {
+document.addEventListener("DOMContentLoaded", function () {
     const tareaForm = document.getElementById("tareaForm");
     const listaTareas = document.getElementById("listaTareas");
     const totalTareas = document.getElementById("totalTareas");
     const mensajeH2 = document.getElementById("mensaje");
     const paisSelect = document.getElementById("pais");
+    const calendario = document.getElementById("calendario");
 
-    const tareas = JSON.parse(localStorage.getItem("tareas")) || [];
+    let tareas = JSON.parse(localStorage.getItem("tareas")) || [];
 
-    actualizarListaTareas();
+    function actualizarCalendario() {
+        calendario.innerHTML = "";
+        const countryCode = paisSelect.value;
 
-    tareaForm.addEventListener("submit", async function (event) {
-        event.preventDefault();
+        // Obtener días festivos
+        obtenerDiasFestivos(countryCode).then(function (holidays) {
+            // Crear un array de eventos para Simple Calendar
+            const events = holidays.map(function (holiday) {
+                return {
+                    startDate: `${holiday.date}T00:00:00`,
+                    endDate: `${holiday.date}T23:59:59`,
+                    summary: `[Feriado] ${holiday.name}`,
+                };
+            });
 
-        try {
-            const titulo = document.getElementById("titulo").value;
-            const descripcion = document.getElementById("descripcion").value;
-            const hora = document.getElementById("hora").value;
-            const countryCode = paisSelect.value;
+            // Agregar eventos de tareas a la lista
+            tareas.forEach(function (tarea) {
+                const fechaTarea = new Date(`${tarea.fecha}T${tarea.hora}:00`);
+                events.push({
+                    startDate: fechaTarea.toISOString(),
+                    endDate: fechaTarea.toISOString(),
+                    summary: `[Tarea] ${tarea.titulo}`,
+                });
+            });
 
-            if (!titulo || !descripcion || !hora) {
-                throw new Error("Todos los campos deben estar llenos");
-            }
+            // Configurar Simple Calendar
+            const calendar = new SimpleCalendar({
+                container: "#calendario",
+                month: new Date().getMonth() + 1,
+                year: new Date().getFullYear(),
+                displayYear: true,
+                disableYearDropdown: true,
+                disableTimezone: true,
+                eventsData: events,
+                onClickDate: function (date) {
+                    // Cuando se hace clic en una fecha, mostrar las tareas correspondientes
+                    const tareasFecha = tareas.filter(function (tarea) {
+                        const fechaTarea = new Date(`${tarea.fecha}T${tarea.hora}:00`);
+                        return fechaTarea.toISOString().split('T')[0] === date;
+                    });
 
-            const holidays = await obtenerDiasFestivos(countryCode);
-            
-            const tarea = {
-                titulo: titulo,
-                descripcion: descripcion,
-                hora: hora,
-                fechaCreacion: obtenerFechaHoraActual(),
-                diasFestivos: holidays,
-            };
+                    mostrarTareasFecha(tareasFecha);
+                }
+            });
+        });
+    }
 
-            tareas.push(tarea);
-            localStorage.setItem("tareas", JSON.stringify(tareas));
-            tareaForm.reset();
-            actualizarListaTareas();
-            mostrarCalendario();
-        } catch (error) {
-            mostrarError(error.message);
-        }
-    });
-
-    async function obtenerDiasFestivos(countryCode) {
+    function obtenerDiasFestivos(countryCode) {
         const year = new Date().getFullYear();
-        const response = await fetch(`https://date.nager.at/api/v3/PublicHolidays/${year}/${countryCode}`);
-        const holidays = await response.json();
-        return holidays.map(holiday => holiday.date);
-    }
-
-    function mostrarError(mensaje) {
-        mensajeH2.textContent = mensaje;
-        setTimeout(() => {
-            mensajeH2.textContent = "";
-        }, 5000);
-    }
-
-    function obtenerFechaHoraActual() {
-        const ahora = new Date();
-        const fechaHora = ahora.toLocaleString();
-        return fechaHora;
+        return fetch(`https://date.nager.at/api/v3/PublicHolidays/${year}/${countryCode}`)
+            .then(response => response.json());
     }
 
     function actualizarListaTareas() {
         listaTareas.innerHTML = "";
 
-        tareas.forEach(function (tarea, indice) {
+        tareas.forEach(function (tarea) {
             const tareaItem = document.createElement("li");
-            tareaItem.innerHTML = `<strong>${tarea.titulo}</strong><br>${tarea.descripcion}<br>Hora: ${tarea.hora}<br>Fecha de Creación: ${tarea.fechaCreacion}`;
+            tareaItem.innerHTML = `<strong>${tarea.titulo}</strong><br>${tarea.descripcion}<br>Fecha: ${tarea.fecha}<br>Hora: ${tarea.hora}`;
 
             const botonCompletar = document.createElement("button");
             botonCompletar.textContent = "Completar";
             botonCompletar.addEventListener("click", function () {
-                try {
-                    tareas.splice(indice, 1);
-                    localStorage.setItem("tareas", JSON.stringify(tareas));
-                    actualizarListaTareas();
-                    mostrarCalendario();
-                    mensajeH2.textContent = "Tarea completada";
-                } catch (error) {
-                    mostrarError(error.message);
-                }
+                completarTarea(tarea);
             });
 
             tareaItem.appendChild(botonCompletar);
@@ -97,25 +87,73 @@ document.addEventListener("DOMContentLoaded", async function () {
         }
     }
 
-    async function mostrarCalendario() {
-        const countryCode = paisSelect.value;
-        const fecha = new Date();
-        const diasFestivos = await obtenerDiasFestivos(countryCode);
+    function mostrarError(mensaje) {
+        mensajeH2.textContent = mensaje;
+        setTimeout(() => {
+            mensajeH2.textContent = "";
+        }, 5000);
+    }
 
-        const calendarioDiv = document.getElementById("calendario");
-        calendarioDiv.innerHTML = "";
+    function mostrarTareasFecha(tareasFecha) {
+        listaTareas.innerHTML = "";
 
-        for (let i = 1; i <= 31; i++) {
-            const dia = new Date(fecha.getFullYear(), fecha.getMonth(), i);
-            const esFestivo = diasFestivos.includes(dia.toISOString().split('T')[0]);
+        tareasFecha.forEach(function (tarea) {
+            const tareaItem = document.createElement("li");
+            tareaItem.innerHTML = `<strong>${tarea.titulo}</strong><br>${tarea.descripcion}<br>Fecha: ${tarea.fecha}<br>Hora: ${tarea.hora}`;
 
-            const span = document.createElement("span");
-            span.textContent = i;
-            if (esFestivo) {
-                span.classList.add("festivo");
+            const botonCompletar = document.createElement("button");
+            botonCompletar.textContent = "Completar";
+            botonCompletar.addEventListener("click", function () {
+                completarTarea(tarea);
+            });
+
+            tareaItem.appendChild(botonCompletar);
+            listaTareas.appendChild(tareaItem);
+        });
+    }
+
+    function completarTarea(tarea) {
+        const index = tareas.findIndex(function (t) {
+            return t.fecha === tarea.fecha && t.hora === tarea.hora && t.titulo === tarea.titulo && t.descripcion === tarea.descripcion;
+        });
+        tareas.splice(index, 1);
+        localStorage.setItem("tareas", JSON.stringify(tareas));
+        actualizarCalendario();
+        actualizarListaTareas();
+    }
+
+    tareaForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        try {
+            const fecha = document.getElementById("fecha").value;
+            const hora = document.getElementById("hora").value;
+            const titulo = document.getElementById("titulo").value;
+            const descripcion = document.getElementById("descripcion").value;
+
+            if (!fecha || !hora || !titulo || !descripcion) {
+                throw new Error("Todos los campos deben estar llenos");
             }
 
-            calendarioDiv.appendChild(span);
+            const tarea = {
+                fecha: fecha,
+                hora: hora,
+                titulo: titulo,
+                descripcion: descripcion,
+            };
+
+            tareas.push(tarea);
+            localStorage.setItem("tareas", JSON.stringify(tareas));
+            tareaForm.reset();
+
+            actualizarCalendario();
+            actualizarListaTareas();
+        } catch (error) {
+            mostrarError(error.message);
         }
-    }
+    });
+
+    // Inicializar la aplicación
+    actualizarCalendario();
+    actualizarListaTareas();
 });
