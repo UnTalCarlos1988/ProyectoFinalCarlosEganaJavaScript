4837bf5db84de7371d4459cd9f763bcdfebd4fa5
carlos egana <carlos.egana.gutierrez@gmail.com>
2023-11-24 18:12:44 -0300

    commit final1

--- main4.js
+++ main4.js
@@ -1,59 +1,48 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", async function () {
     const tareaForm = document.getElementById("tareaForm");
     const listaTareas = document.getElementById("listaTareas");
     const totalTareas = document.getElementById("totalTareas");
     const mensajeH2 = document.getElementById("mensaje");
     const paisSelect = document.getElementById("pais");
-    const calendario = document.getElementById("calendario");
+    
+    const festivosLista = document.getElementById("festivosLista");
 
-    let tareas = JSON.parse(localStorage.getItem("tareas")) || [];
+    const tareas = JSON.parse(localStorage.getItem("tareas")) || [];
 
-    function actualizarCalendario() {
-        calendario.innerHTML = "";
-        const countryCode = paisSelect.value;
+    async function obtenerDiasFestivos(year, month, countryCode) {
+        const response = await fetch(`https://date.nager.at/api/v3/PublicHolidays/${year}/${countryCode}`);
+        const holidays = await response.json();
 
-        obtenerDiasFestivos(countryCode).then(function (holidays) {
-            const events = holidays.map(function (holiday) {
-                return {
-                    startDate: `${holiday.date}T00:00:00`,
-                    endDate: `${holiday.date}T23:59:59`,
-                    summary: `[Feriado] ${holiday.name}`,
-                };
-            });
-
-            tareas.forEach(function (tarea) {
-                const fechaTarea = new Date(`${tarea.fecha}T${tarea.hora}:00`);
-                events.push({
-                    startDate: fechaTarea.toISOString(),
-                    endDate: fechaTarea.toISOString(),
-                    summary: `[Tarea] ${tarea.titulo}`,
-                });
-            });
-
-            const calendar = new SimpleCalendar({
-                container: "#calendario",
-                month: new Date().getMonth() + 1,
-                year: new Date().getFullYear(),
-                displayYear: true,
-                disableYearDropdown: true,
-                disableTimezone: true,
-                eventsData: events,
-                onClickDate: function (date) {
-                    const tareasFecha = tareas.filter(function (tarea) {
-                        const fechaTarea = new Date(`${tarea.fecha}T${tarea.hora}:00`);
-                        return fechaTarea.toISOString().split('T')[0] === date;
-                    });
-
-                    mostrarTareasFecha(tareasFecha);
-                }
-            });
+        const festivosMes = holidays.filter(holiday => {
+            const date = new Date(holiday.date);
+            return date.getMonth() === month - 1;
         });
+
+        return festivosMes;
     }
 
-    function obtenerDiasFestivos(countryCode) {
+    async function actualizarDiasFestivos() {
+        festivosLista.innerHTML = "";
+
+        const countryCode = paisSelect.value;
         const year = new Date().getFullYear();
-        return fetch(`https://date.nager.at/api/v3/PublicHolidays/${year}/${countryCode}`)
-            .then(response => response.json());
+        const month = new Date().getMonth() + 1;
+
+        try {
+            const holidays = await obtenerDiasFestivos(year, month, countryCode);
+
+            if (holidays.length > 0) {
+                festivosLista.innerHTML = "<ul>";
+                holidays.forEach(holiday => {
+                    festivosLista.innerHTML += `<li>${holiday.date}: ${holiday.name}</li>`;
+                });
+                festivosLista.innerHTML += "</ul>";
+            } else {
+                festivosLista.innerHTML = "<p>No hay días festivos para el mes en curso.</p>";
+            }
+        } catch (error) {
+            mostrarError("Error al obtener días festivos: " + error.message);
+        }
     }
 
     function actualizarListaTareas() {
@@ -89,31 +78,18 @@ document.addEventListener("DOMContentLoaded", function () {
         }, 5000);
     }
 
-    function mostrarTareasFecha(tareasFecha) {
-        listaTareas.innerHTML = "";
-
-        tareasFecha.forEach(function (tarea) {
-            const tareaItem = document.createElement("li");
-            tareaItem.innerHTML = `<strong>${tarea.titulo}</strong><br>${tarea.descripcion}<br>Fecha: ${tarea.fecha}<br>Hora: ${tarea.hora}`;
-
-            const botonCompletar = document.createElement("button");
-            botonCompletar.textContent = "Completar";
-            botonCompletar.addEventListener("click", function () {
-                completarTarea(tarea);
-            });
-
-            tareaItem.appendChild(botonCompletar);
-            listaTareas.appendChild(tareaItem);
-        });
-    }
-
     function completarTarea(tarea) {
         const index = tareas.findIndex(function (t) {
-            return t.fecha === tarea.fecha && t.hora === tarea.hora && t.titulo === tarea.titulo && t.descripcion === tarea.descripcion;
+            return (
+                t.fecha === tarea.fecha &&
+                t.hora === tarea.hora &&
+                t.titulo === tarea.titulo &&
+                t.descripcion === tarea.descripcion
+            );
         });
         tareas.splice(index, 1);
         localStorage.setItem("tareas", JSON.stringify(tareas));
-        actualizarCalendario();
+        actualizarDiasFestivos();
         actualizarListaTareas();
     }
 
@@ -141,14 +117,14 @@ document.addEventListener("DOMContentLoaded", function () {
             localStorage.setItem("tareas", JSON.stringify(tareas));
             tareaForm.reset();
 
-            actualizarCalendario();
+            actualizarDiasFestivos();
             actualizarListaTareas();
         } catch (error) {
             mostrarError(error.message);
         }
     });
 
-   
-    actualizarCalendario();
+    
+    await actualizarDiasFestivos();
     actualizarListaTareas();
 });
